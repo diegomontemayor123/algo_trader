36ed047ce4938aa290dd78bd69b70dc171ab7487
diegomontemayor123 <diegomontemayor.f@gmail.com>
2025-07-14 15:42:48 +0100

    l

--- model.py
+++ model.py
@@ -81,7 +81,7 @@ class DifferentiableSharpeLoss(nn.Module):
         return loss
 
 class TransformerLRScheduler(torch.optim.lr_scheduler._LRScheduler):
-    def __init__(self, optimizer, d_model, warmup_steps=50, last_epoch=-1):
+    def __init__(self, optimizer, d_model, warmup_steps, last_epoch=-1):
         self.d_model = d_model
         self.warmup_steps = warmup_steps
         super().__init__(optimizer, last_epoch)
