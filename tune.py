f7cfc7d60cdb9e72eba04333e9a9e578eb4bedd4
diegomontemayor123 <diegomontemayor.f@gmail.com>
2025-07-15 09:00:22 +0100

    l

--- tune.py
+++ tune.py
@@ -68,7 +68,7 @@ def run_experiment(trial):
         avg_benchmark_outperformance = extract_avg_benchmark_outperformance(output)
         if sharpe is None or drawdown is None:
             return -float("inf")
-        score = (1 * sharpe) + (1 * avg_benchmark_outperformance) - (0.7 * abs(drawdown))
+        score = (1 * sharpe) + (1 * avg_benchmark_outperformance) - (0.1 * abs(drawdown))
         trial.set_user_attr("sharpe", sharpe)
         trial.set_user_attr("drawdown", drawdown)
         trial.set_user_attr("avg_benchmark_outperformance", avg_benchmark_outperformance)
