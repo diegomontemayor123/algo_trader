7f92b41df0fc3fcba562c6aae51b92544c78c954
diegomontemayor123 <diegomontemayor.f@gmail.com>
2025-07-16 12:04:01 +0100

    l

--- tune.py
+++ tune.py
@@ -7,9 +7,9 @@ from optuna.samplers import TPESampler
 
 def run_experiment(trial):
     config = {
-        "START_DATE": trial.suggest_categorical("START_DATE", ["2012-01-01","2013-01-01","2014-01-01","2015-01-01","2016-01-01","2017-01-01"]),
+        "START_DATE": trial.suggest_categorical("START_DATE", ["2012-01-01","2014-01-01","2016-01-01","2017-01-01"]),
         "END_DATE": trial.suggest_categorical("END_DATE", ["2025-07-01"]),
-        "SPLIT_DATE": trial.suggest_categorical("SPLIT_DATE", ["2021-07-01"]),
+        "SPLIT_DATE": trial.suggest_categorical("SPLIT_DATE", ["2021-07-01","2023-07-01"]),
         "TICKERS": trial.suggest_categorical("TICKERS", ['ORCL,GOOGL,JPM,PFE,MSFT,T,KO,UNH,GE,CAT,TSLA,AAPL',
                                                          "AAPL,MSFT,GOOGL,AMZN,META,NVDA,TSLA,JPM,WMT,CVX,MCD,T,NKE",
                                                          "BRK, TXN, HD, T, BA, GS, NVDA, MSFT, LOW, TMO, CAT, XOM, LLY, PEP, AVGO, MCD, COST, MA, ABBV, VZ, BAC, WMT, LMT, JPM, DE",
