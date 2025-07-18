328b363ab2ed2f159bbc83f21153092ad88cefd9
diegomontemayor123 <diegomontemayor.f@gmail.com>
2025-07-18 14:08:22 +0100

    l

--- tune.py
+++ tune.py
@@ -106,7 +106,7 @@ def run_experiment(trial):
 def main():
     sampler = TPESampler(seed=42)
     study = optuna.create_study(direction="maximize", sampler=sampler)
-    study.optimize(run_experiment, n_trials=10, n_jobs=1)
+    study.optimize(run_experiment, n_trials=30, n_jobs=1)
     best = study.best_trial
     best_params = best.params.copy()
     with open("hyparams.json", "w") as f:
