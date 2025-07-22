e302a8d8ebc97050be3e7df270ed11db28131b5a
diegomontemayor123 <diegomontemayor.f@gmail.com>
2025-07-22 12:37:31 +0100

    l

--- tune.py
+++ tune.py
@@ -1,7 +1,7 @@
 import os, subprocess, re, optuna, json, csv
 from optuna.samplers import TPESampler
 
-TRIALS = 20
+TRIALS = 10
 
 def run_experiment(trial):
     config = {"START": trial.suggest_categorical("START", ["2019-01-01"]),
@@ -19,8 +19,8 @@ def run_experiment(trial):
         "ATTENT": trial.suggest_categorical("ATTENT", [0]),
         "FEAT_PER": trial.suggest_categorical("FEAT_PER", ["8,12,24"]),
         "INIT_LR": trial.suggest_float("INIT_LR", 0.01, 0.01),
-        "EXP_PEN": trial.suggest_float("EXP_PEN", 0.001, 0.01),#0.006/0.0038/0.0058 linear/exp/sddenom
-        "EXP_EXP": trial.suggest_float("EXP_EXP", 1.6, 2),
+        "EXP_PEN": trial.suggest_float("EXP_PEN", 0.01, 0.04),#0.006/0.0038/0.0058 linear/exp/sddenom
+        "EXP_EXP": trial.suggest_float("EXP_EXP", 1.8, 1.8),
         "RETURN_PEN": trial.suggest_float("RETURN_PEN", 0.38,0.38),#0.18/0.118/0.38 linear/exp/sddenom
         "RETURN_EXP": trial.suggest_float("RETURN_EXP", 0.32,0.32),
         "SD_PEN": trial.suggest_float("SD_PEN", 0.14,0.14),#0.157/0.12 exp/sddenom
