1a886b0827abbc236a0dda18b9069bfde7b8fcdb
diegomontemayor123 <diegomontemayor.f@gmail.com>
2025-07-22 11:28:02 +0100

    l

--- tune.py
+++ tune.py
@@ -24,7 +24,7 @@ def run_experiment(trial):
         "RETURN_PEN": trial.suggest_float("RETURN_PEN", 0.118,0.5),#0.18/0.118 linear/exp
         "RETURN_EXP": trial.suggest_float("RETURN_EXP", 1,1),#0.329
         "SD_PEN": trial.suggest_float("SD_PEN", 0,0),#0.157
-        "SD_EXP": trial.suggest_float("SD_EXP",0.6,1.1),#0.776
+        "SD_EXP": trial.suggest_float("SD_EXP",1,1),#0.776
         "TEST_CHUNK": trial.suggest_int("TEST_CHUNK", 12, 12),
         "RETRAIN_WIN": trial.suggest_int("RETRAIN_WIN", 0, 0),
         "SEED": trial.suggest_int("SEED", 42, 42),
