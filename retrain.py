04d1c808c17b5c95391eb366328960fcbbea3969
diegomontemayor123 <diegomontemayor.f@gmail.com>
2025-07-25 13:22:38 +0100

    l

--- retrain.py
+++ retrain.py
@@ -30,11 +30,11 @@ def run_retraining_chunks(chunks, feat_df, ret_df, lback, norm_feat, TICK, feat,
             print("[Info] Skipping retraining for first chunk — using initial model0.")
             current_model = model0
         else:
-            original_train_length = (chunks[0][0] - pd.to_datetime(start))
+            orig_train = (chunks[0][0] - pd.to_datetime(start))
             train_end = chunk_start - pd.Timedelta(days=1)
-            train_start = max(train_end - original_train_length, pd.to_datetime(start))
+            train_start = max(train_end - orig_train, pd.to_datetime(start))
             training_days = (train_end - train_start).days
-            if training_days < original_train_length.days - 5:
+            if training_days < orig_train.days - 5:
                 print(f"Skipping chunk {idx+1} due to short training window: {training_days} days")
                 continue
 
