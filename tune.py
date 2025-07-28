741b7558a9b6fa9d795353a2ba9ee1fe6b652863
diegomontemayor123 <diegomontemayor.f@gmail.com>
2025-07-28 14:12:39 +0100

    l

--- tune.py
+++ tune.py
@@ -15,12 +15,12 @@ def run_experiment(trial):
         "BATCH": trial.suggest_int("BATCH",53,53),#53
         "LBACK": trial.suggest_int("LBACK",84,84),#84
         "PRED_DAYS": trial.suggest_int("PRED_DAYS",3,3),#6
-        "DROPOUT": trial.suggest_float("DROPOUT",.0227,.028),#.028
-        "DECAY": trial.suggest_float("DECAY",.0021,.003),#.003
+        "DROPOUT": trial.suggest_float("DROPOUT",.028,.028),#.028
+        "DECAY": trial.suggest_float("DECAY",.003,.003),#.003
         "FEAT_PER": trial.suggest_categorical("FEAT_PER", ["8,12,24"]),
-        "INIT_LR": trial.suggest_float("INIT_LR",.0045,.006),#.006
+        "INIT_LR": trial.suggest_float("INIT_LR",.006,.006),#.006
         "EXP_PEN": trial.suggest_float("EXP_PEN",.226,.226),#.235 price,ema,vix     / .226 long macro/feat
-        "EXP_EXP": trial.suggest_float("EXP_EXP",1.82,1.82),#1.82
+        "EXP_EXP": trial.suggest_float("EXP_EXP",1.72,1.92),#1.82
         "RETURN_PEN": trial.suggest_float("RETURN_PEN",.07,.07),#.105 price,ema,vix / .07 long macro/feat
         "RETURN_EXP": trial.suggest_float("RETURN_EXP",.28,.28),#.28 
         "SD_PEN": trial.suggest_float("SD_PEN",.17,.17),#.17 
