d69ffc5dd07999b46238fcb57a76f4f237d1843c
diegomontemayor123 <diegomontemayor.f@gmail.com>
2025-07-30 21:43:34 +0100

    l

--- filter.py
+++ filter.py
@@ -6,45 +6,30 @@ from load import load_config
 config = load_config()
 
 def select_features(feat, ret, split_date, thresh=config["THRESH"], method=config["FILTER"]):
-    if method is None: 
-        return feat
+    if method is None: return feat
     method = method[0]
     split_date_ts = pd.to_datetime(split_date)
     start = split_date_ts - pd.DateOffset(months=config["FILTERWIN"])
-
     mask = ret.notna().all(axis=1)
     mask = mask & (mask.index < split_date_ts) & (mask.index >= start)
-
-
     def max_drawdown(returns): cum = (1 + returns).cumprod();drawdown = (cum - cum.cummax()) / cum.cummax();return drawdown.min()
     window = config["YWIN"] 
-    y = (ret.loc[mask].mean(axis=1).shift(-window).rolling(window).mean() - (-ret.loc[mask].mean(axis=1).shift(-window).rolling(window).apply(max_drawdown, raw=False) + 1e-10)).dropna()
+    y = (ret.loc[mask].mean(axis=1).shift(-window).rolling(window).mean() - (ret.loc[mask].mean(axis=1).shift(-window).rolling(window).apply(max_drawdown, raw=False) + 1e-10)).dropna()
     #y = ret.loc[mask].mean(axis=1) / (ret.loc[mask].std(axis=1) + 1e-10)
     X = feat.loc[y.index]
-
     if method == "rf":
         model = RandomForestRegressor(n_estimators=config["NESTIM"], random_state=config["SEED"])
         model.fit(X, y)
         scores = pd.Series(model.feature_importances_, index=X.columns)
-    elif method == "mutual":
-        scores = pd.Series(mutual_info_regression(X, y, random_state=config["SEED"]), index=X.columns)
-    elif method == "correl":
-        scores = X.apply(lambda col: col.corr(y)).abs()
-    else:
-        print(f"[Filter] Unknown method '{method}' specified, skipping filtering")
-        return feat
-
-    score_name = method
-
+    elif method == "mutual": scores = pd.Series(mutual_info_regression(X, y, random_state=config["SEED"]), index=X.columns)
+    elif method == "correl": scores = X.apply(lambda col: col.corr(y)).abs()
+    else: print(f"[Filter] Unknown method '{method}' specified, skipping filtering");return feat
     if thresh > 1:
         selected_features = scores.nlargest(int(thresh)).index
-        print(f"[Filter] Selected top {int(thresh)} features by {score_name} between {start.date()} - {split_date_ts.date()}")
+        print(f"[Filter] Selected top {int(thresh)} features by {method} between {start.date()} - {split_date_ts.date()}")
         print(f"[Filter] x/y date range: {X.index.min().date()} to {X.index.max().date()} / {y.index.min().date()} to {y.index.max().date()}")
     elif 0 < thresh <= 1:
         selected_features = scores[scores > thresh].index
-        print(f"[Filter] Selected {len(selected_features)} features with {score_name} > {thresh} between {start.date()} - {split_date_ts.date()}")
+        print(f"[Filter] Selected {len(selected_features)} features with {method} > {thresh} between {start.date()} - {split_date_ts.date()}")
         print(f"[Filter] x/y date range: {X.index.min().date()} to {X.index.max().date()} / {y.index.min().date()} to {y.index.max().date()}")
-        
-
-
     return feat[selected_features]
--- tune.py
+++ tune.py
@@ -22,13 +22,13 @@ def run_experiment(trial,study=None):
         "DECAY": trial.suggest_float("DECAY",.003,.003),#.003
         "FEAT_PER": trial.suggest_categorical("FEAT_PER", ["8,12,24"]),
         "INIT_LR": trial.suggest_float("INIT_LR",.006,.006),#.006
-        "EXP_PEN": trial.suggest_float("EXP_PEN",.226,.226),#.235 price,ema,vix     / .226 long macro/feat
+        "EXP_PEN": trial.suggest_float("EXP_PEN",.226,.226),#.226 long macro/feat
         "EXP_EXP": trial.suggest_float("EXP_EXP",1.8,1.8),#1.8
-        "RETURN_PEN": trial.suggest_float("RETURN_PEN",.07,.07),#.105 price,ema,vix / .07 long macro/feat
+        "RETURN_PEN": trial.suggest_float("RETURN_PEN",.07,.07),#.07 long macro/feat
         "RETURN_EXP": trial.suggest_float("RETURN_EXP",.28,.28),#.28 
         "SD_PEN": trial.suggest_float("SD_PEN",.17,.17),#.17 
         "SD_EXP": trial.suggest_float("SD_EXP",.74,.74),#.74 
-        "SEED": trial.suggest_categorical("SEED",[42]),
+        "SEED": trial.suggest_categorical("SEED",[42]),#42
         "MAX_HEADS": trial.suggest_categorical("MAX_HEADS", [1]),#1
         "LAYERS": trial.suggest_categorical("LAYERS", [1]),#1
         "EARLY_FAIL": trial.suggest_categorical("EARLY_FAIL",[2]),#2
