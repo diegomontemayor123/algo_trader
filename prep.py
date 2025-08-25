8b915dc1eb831fe8db1caebea64833297a468533
Diego <diegomontemayor.f@gmail.com>
2025-08-25 17:07:50 +0100

    l

--- prep.py
+++ prep.py
@@ -53,6 +53,10 @@ def zscore_shrink(local_seq, global_seq, anchor_seq, alpha=None, beta=None):
     local_z = (local_seq - local_seq.mean(axis=0, keepdims=True)) / (local_seq.std(axis=0, keepdims=True) + 1e-8)
     anchor_seq = np.broadcast_to(anchor_seq, local_seq.shape)
     return alpha * local_z + (1 - alpha) * (1-beta) * global_seq + beta * anchor_seq
+    #anchor_z = (anchor_seq - anchor_seq.mean(axis=0, keepdims=True)) / (anchor_seq.std(axis=0, keepdims=True) + 1e-8)
+    #return alpha * local_z + (1 - alpha) * (1 - beta) * global_seq + beta * anchor_z
+
+
 
 def prep_data(feat, ret, config, anchor_date=None):
     print(f"[Prep] Feat/Ret date range: {feat.index[0].date()} - {feat.index[-1].date()} / {ret.index[0].date()} - {ret.index[-1].date()}")
